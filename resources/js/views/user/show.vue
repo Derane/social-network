110d30efcf516ba0c3cc9e030cdd23201a5da66b
Valerii Sierhieiev <vsierhieiev-its24@lll.kpi.ua>
2023-09-19 14:28:05 +0300

    add show users

---
+++ resources/js/views/user/show.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="w-96 mx-auto">
+
+        <div v-if="posts">
+            <Post v-for="post in posts" :post="post"></Post>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import Post from "../../components/Post.vue";
+
+export default {
+    name: "Personal",
+
+    data() {
+        return {
+            posts: [],
+            userId: this.$route.params.id,
+        }
+    },
+
+    mounted() {
+        this.getPosts()
+    },
+    components: {
+        Post
+    },
+    methods: {
+
+        getPosts() {
+            axios.get(`/api/users/${this.userId}/posts`)
+                .then(res => {
+                    this.posts = res.data.data
+                })
+        },
+
+
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
